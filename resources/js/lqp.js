80b5398f4155e87e069b666ffe6e6313416eaaf9
喵才人 <oncwnuIa6v2kJ7CqHUYO8yNfkgD8@git.weixin.qq.com>
2022-04-08 14:49:58 +0800

    封装导出

--- resources/js/lqp.js
+++ resources/js/lqp.js
@@ -1,45 +1,42 @@
 import {ref} from "vue";
 import {ElMessageBox} from "element-plus";
-
 const aa = 'aaa';
-
-
 function bb () {
     return 'bb';
 }
+//全部导出
 const excelData = ref([])
-const excelDataSelect = ref([])
-const allExportExcel = () => {
-    //全部导出
+const allExportExcel = (tData, tTitle, tName) => {
     // 数据写入excel
-    ElMessageBox.confirm('将导出为excel文件，确认导出?').then(() => {
-        if(excelDataSelect.value.length > 0){
-            excelData.value = excelDataSelect.value
-        }else{
-            excelData.value = tableData
+    ElMessageBox.confirm('将导出为excel文件，确认导出?',
+        {
+            confirmButtonText: '导出',
+            cancelButtonText: '取消',
+            type: 'success',
         }
-        console.log('excelData.value', excelData.value)
-        export2Excel()
+        ).then(() => {
+        excelData.value = tData
+        export2Excel(tData, tTitle, tName)
     }).catch(() => {
 
     })
 }
-const export2Excel = () => {
+const export2Excel = (tData, tTitle, tName) => {
     require.ensure([], () => {
         const { export_json_to_excel } = require('@/Pages/excel/export2Excel') // 这里必须使用绝对路径，使用@/+存放export2Excel的路径
-        const tHeader = ['账号', '密码', '公司名称', '小号', '坐席', '限制用户', '费率（元）', '结束时间'] // 导出的excel的表头字段
-        const filterVal = ['number','password','name', 'minNumber', 'sit', 'limitNumber', 'rate', 'dataTime'] // 对象属性，对应于tHeader
+        const tHeader = [] // 导出的excel的表头字段名称
+        const filterVal = [] // 对象属性，对应于tHeader
+        tTitle.forEach((item)=>{
+            tHeader.push(item.label)
+            filterVal.push(item.value)
+        })
         const list = excelData.value //json数组对象，接口返回的数据
         const data = formatJson(filterVal, list)
-        export_json_to_excel(tHeader, data, '检测单体数据')// 导出的表格名称
+        export_json_to_excel(tHeader, data, tName)// 导出的表格名称
     })
 }
 const formatJson = (filterVal, jsonData) => {
     return jsonData.map(v => filterVal.map(j => v[j]))
 }
-const selectExportData = (value) => {
-    // 选择导出
-    console.log('拿到复选框', value)
-    excelDataSelect.value = value
-}
+// 其他方法
 export { aa, bb, allExportExcel };
