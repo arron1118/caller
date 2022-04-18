a0b998e37b8f576c6ed7789ef07558ad2e92c688
arron1118 <arron1118@icloud.com>
2022-04-18 18:03:01 +0800

    update

--- resources/js/http/request.js
+++ resources/js/http/request.js
@@ -1,4 +1,32 @@
 import axios from 'axios'
+import { Inertia } from '@inertiajs/inertia'
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+
+    if (response.data.code === 5003) {
+        route().current()
+        Inertia.get(route('admin.login'));
+    }
+
+    return response;
+}, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
+
 function post(url,par){
     return new Promise(((resolve, reject) => {
         axios.post(route(url), par).then(res => {
