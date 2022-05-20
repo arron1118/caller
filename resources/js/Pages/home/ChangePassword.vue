82577cc5b6c53d9982a34a918c10e13240e34083
喵才人 <oncwnuIa6v2kJ7CqHUYO8yNfkgD8@git.weixin.qq.com>
2022-05-20 18:01:57 +0800

    89

--- resources/js/Pages/home/ChangePassword.vue
+++ resources/js/Pages/home/ChangePassword.vue
@@ -1,24 +1,113 @@
 <template>
     <home-layout>
-        passward
+        <div class="grid grid-cols-4">
+            <div class="col-span-1"></div>
+            <div class="col-span-2 bg-white rounded shadow p-4">
+                <el-form ref="addFormRef" :model="ruleForm">
+                    <el-form-item label="旧的密码" label-width="140px" prop="old_password">
+                        <el-input v-model.trim="ruleForm.old_password" type="text" placeholder="请输入旧的密码" />
+                    </el-form-item>
+                    <el-form-item label="新的密码" label-width="140px" prop="new_password">
+                        <el-input v-model.trim="ruleForm.new_password" type="text" placeholder="请输入新的密码" />
+                    </el-form-item>
+                    <el-form-item label="确认密码" label-width="140px" prop="confirm_password">
+                        <el-input v-model.trim="ruleForm.confirm_password" type="text" placeholder="请输入确认密码" />
+                    </el-form-item>
+                    <!--          提交-->
+                    <div class="flex flex-row justify-center mt-8">
+                        <el-button @click="cancelAdd()">取消</el-button>
+                        <el-button type="primary" :loading="loading" @click="submitAdd()"
+                        >保存</el-button
+                        >
+                    </div>
+                </el-form>
+            </div>
+            <div class="col-span-1"></div>
+        </div>
     </home-layout>
 </template>
 
 <script>
 import HomeLayout from "@/Layouts/HomeLayout";
-import { ref } from "vue"
+import {reactive, ref, unref} from "vue"
 import {ElMessage} from "element-plus";
+import { post } from "@/http/request"
 export default {
-    name: "ChangePassword",
+    name: "Material",
     components: {
         HomeLayout,
     },
     setup(){
-        const loading = ref(false)
-        return{
-            loading
+        const materialData = ref([])
+        const url = ref('getHistoryList')
+        const where = ref({
+            limit: 1,
+            page: 1
+        })
+        const getData =  () => {
+            post(url.value, where.value).then((res)=> {
+                console.log('ziliao', res)
+                materialData.value = res.data[0]
+            })
         }
-    }
+        const addFormRef = ref(null)
+        const loading = ref(false)
+        const ruleForm = reactive({
+            old_password: '',
+            new_password: '',
+            confirm_password: ''
+        })
+        const submitAdd = async () => {
+            loading.value = true
+            const form = unref(addFormRef)
+            if (!form) return
+            try {
+                await form.validate()
+                const {
+                    old_password,
+                    new_password,
+                    confirm_password
+                } = ruleForm
+                const params = {
+                    old_password: old_password,
+                    new_password: new_password,
+                    confirm_password: confirm_password
+                }
+                console.log('开通参数', params)
+                setTimeout(function () {
+                    loading.value = false
+                    ElMessage({
+                        type: 'success',
+                        // message: `action: ${action}`,
+                        message: '已提交'
+                    })
+                    // 重载表格数据
+
+                }, 3000);
+
+                // todo
+            } catch (error) {
+            }
+        }
+        const cancelAdd = async () => {
+            loading.value = false
+            return false
+        }
+        return{
+            materialData,
+            url,
+            where,
+            getData,
+            addFormRef,
+            ruleForm,
+            loading,
+            submitAdd,
+            cancelAdd,
+        }
+    },
+    mounted() {
+        this.getData()
+    },
 }
 </script>
 
--- resources/js/Pages/home/Material.vue
+++ resources/js/Pages/home/Material.vue
@@ -5,25 +5,24 @@
             <div class="col-span-2 bg-white rounded shadow p-4">
                 <el-form ref="addFormRef" :model="ruleForm">
                     <el-form-item label="昵称" label-width="140px" prop="username">
-                        <el-input v-model.trim="ruleForm.username" type="text" autocomplete="off" />
-                        <div class="text-gray-400">注：手机号用于拨号，请认真填写</div>
+                        <el-input v-model="materialData.username" type="text" disabled />
+                        <div class="text-gray-400">注：此昵称作为登录账号使用，暂不支持更改</div>
                     </el-form-item>
                     <el-form-item label="真实姓名" label-width="140px" prop="realname">
-                        <el-input v-model.trim="ruleForm.realname" type="text" autocomplete="off" />
+                        <el-input v-model.trim="materialData.realname" type="text" placeholder="请输入真实姓名" />
                     </el-form-item>
                     <el-form-item label="手机号" label-width="140px" prop="phone">
-                        <el-input v-model.number="ruleForm.phone" v-model.trim="ruleForm.phone" type="text" autocomplete="off" />
+                        <el-input v-model.number="materialData.phone" type="text" />
                         <div class="text-gray-400">注：手机号用于拨号，请认真填写</div>
                     </el-form-item>
-
                     <el-form-item label="登陆IP" label-width="140px" prop="ip">
-                        <el-input v-model.trim="ruleForm.ip" type="text" disabled />
+                        <el-input v-model.trim="materialData.ip" type="text" disabled />
                     </el-form-item>
                     <el-form-item label="最后登陆" label-width="140px" prop="time">
-                        <el-input v-model.trim="ruleForm.time" type="text" disabled />
+                        <el-input v-model.trim="materialData.time" type="text" disabled />
                     </el-form-item>
                     <el-form-item label="上次登陆" label-width="140px" prop="lasttime">
-                        <el-input v-model.trim="ruleForm.lasttime" type="text" disabled />
+                        <el-input v-model.trim="materialData.lasttime" type="text" disabled />
                     </el-form-item>
                     <!--          提交-->
                     <div class="flex flex-row justify-center mt-8">
@@ -43,22 +42,28 @@
 import HomeLayout from "@/Layouts/HomeLayout";
 import {reactive, ref, unref} from "vue"
 import {ElMessage} from "element-plus";
+import { post } from "@/http/request"
 export default {
     name: "Material",
     components: {
         HomeLayout,
     },
     setup(){
+        const materialData = ref([])
+        const url = ref('getHistoryList')
+        const where = ref({
+            limit: 1,
+            page: 1
+        })
+        const getData =  () => {
+            post(url.value, where.value).then((res)=> {
+                console.log('ziliao', res)
+                materialData.value = res.data[0]
+            })
+        }
         const addFormRef = ref(null)
         const loading = ref(false)
-        const ruleForm = reactive({
-            username: '',
-            realname: '',
-            phone: '',
-            ip: '',
-            time: '',
-            lasttime: ''
-        })
+        const ruleForm = reactive(materialData.value)
         const submitAdd = async () => {
             loading.value = true
             const form = unref(addFormRef)
@@ -66,38 +71,56 @@ export default {
             try {
                 await form.validate()
                 const {
-                    cate,
-                    title,
+                    username,
+                    realname,
                     phone,
-                    province,
-                    email,
-                    comment,
+                    ip,
+                    time,
+                    lasttime
                 } = ruleForm
                 const params = {
-                    cate: cate,
-                    title: title,
+                    username: username,
+                    realname: realname,
                     phone: phone,
-                    province: province,
-                    email: email,
-                    comment: comment
+                    ip: ip,
+                    time: time,
+                    lasttime: lasttime
                 }
-                console.log('开通参数', params, loading.value)
-                context.emit('clickAdd', params, loading.value)
+                console.log('开通参数', params)
+                setTimeout(function () {
+                    loading.value = false
+                    ElMessage({
+                        type: 'success',
+                        // message: `action: ${action}`,
+                        message: '已提交'
+                    })
+                    // 重载表格数据
+
+                }, 3000);
+
                 // todo
             } catch (error) {
             }
         }
         const cancelAdd = async () => {
-            context.emit('clickCancelAdd', false)
+            loading.value = false
+            return false
         }
         return{
+            materialData,
+            url,
+            where,
+            getData,
             addFormRef,
             ruleForm,
             loading,
             submitAdd,
             cancelAdd,
         }
-    }
+    },
+    mounted() {
+        this.getData()
+    },
 }
 </script>
 
